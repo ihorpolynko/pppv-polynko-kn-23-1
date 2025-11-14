e403c1312ac7ffee1544d03930167b8810f10e48
Ігор Полинько <igor.polynko@gmail.com>
2025-11-14 21:29:19 +0200

    Added LB3

---
+++ 1-3/Calculator/Calculator/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorExample
+{
+    public interface ICalculator
+    {
+        double Add(double a, double b);
+        double Subtract(double a, double b);
+        double Multiply(double a, double b);
+        double Divide(double a, double b);
+    }
+}
---
+++ 1-3/ClanBattle/ClanBattle/Decorators/UnitDecoratorBase.cs
@@ -0,0 +1,30 @@
+﻿using ClanBattle.Models;
+
+namespace ClanBattle.Decorators
+{
+    public abstract class UnitDecoratorBase : IUnit
+    {
+        protected readonly IUnit _unit;
+
+        protected UnitDecoratorBase(IUnit unit)
+        {
+            _unit = unit;
+        }
+
+        public string Name { get => _unit.Name; set => _unit.Name = value; }
+        public string Weapon { get => _unit.Weapon; set => _unit.Weapon = value; }
+        public string MoveType { get => _unit.MoveType; set => _unit.MoveType = value; }
+        public int Health { get => _unit.Health; set => _unit.Health = value; }
+
+        public int X { get => (_unit as UnitBase).X; set => (_unit as UnitBase).X = value; }
+        public int Y { get => (_unit as UnitBase).Y; set => (_unit as UnitBase).Y = value; }
+
+        public virtual IUnit Clone() => _unit.Clone();
+
+        public virtual void Display() => _unit.Display();
+
+        public virtual double DamageModifier() => 1.0;
+        public virtual double DodgeModifier() => 1.0;
+        public virtual double DefenseModifier() => 1.0;
+    }
+}
